356e4689a0078a5759581f8c60c6478bcaa6f8d4
Tobias Leigh-Wood <tjleighwood@gmail.com>
2023-06-14 11:01:04 +0100

    julia project and docs, r elsa scripts

---
+++ Julia/uk_rsp/src/uk_rsp.jl
@@ -0,0 +1,13 @@
+module uk_rsp
+using Revise
+
+export add_function
+"""
+add_function(a,b)
+
+Returns the sum of two numbers, a and b. 
+
+"""
+add_function(a, b) = return a + b
+
+end # module uk_rsp
