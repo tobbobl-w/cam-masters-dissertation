f064b09b7a0739af3c8b8cd27b2987ac2152f76e
Tobias Leigh-Wood <tjleighwood@gmail.com>
2023-06-15 11:20:36 +0100

    ELSA data work

--- Julia/3_model_play.jl
+++ Julia/3_model_play.jl
@@ -3,3 +3,90 @@
 # Also I should structure this as a julia project from the start otherwise it is going to get messy. 
 # what files do I need to do that?
 
+
+data = ones(10, 10, 10)
+
+typeof(data)
+
+data = [i, j]
+
+
+
+
+
+
+
+
+using FastGaussQuadrature
+using Distributions
+using Plots
+
+# Parameters
+β = 0.9     # Discount factor
+T = 10      # Number of periods
+R_mean = 1.0  # Mean return on savings
+R_std = 0.1   # Standard deviation of return on savings
+
+# Quadrature nodes and weights
+nodes, weights = gausslegendre(10)  # Adjust the number of quadrature nodes as needed
+
+# Define the income distribution
+function income_distribution(t)
+    if t % 2 == 0
+        return Normal(2.0, 0.5)  # Even periods have mean 2.0 and standard deviation 0.5
+    else
+        return Normal(1.0, 0.2)  # Odd periods have mean 1.0 and standard deviation 0.2
+    end
+end
+
+# Value iteration function
+function value_iteration(T, β, R_mean, R_std, nodes, weights)
+    # Initialize value function and policy
+    V = zeros(T + 1)
+    c_policy = zeros(T)
+
+    # Value iteration loop
+    for t = T:-1:1
+        max_value = -Inf
+        max_c = -Inf
+
+        # Iterate over all possible consumption choices
+        for c in 0:0.1:V[t]
+            value = 0.0
+
+            # Iterate over income and interest rate quadrature nodes
+            for (i, node) in enumerate(nodes)
+                income_dist = income_distribution(t)
+                income = node * sqrt(2) * std(income_dist) + mean(income_dist)
+
+                # Calculate utility and future value
+                u = log(c + income)
+                future_value = weights[i] * (V[t+1] * β + c * R_mean)
+
+                # Calculate total value
+                value += weights[i] * (u + β * future_value)
+            end
+
+            # Update maximum value and consumption choice
+            if value > max_value
+                max_value = value
+                max_c = c
+            end
+        end
+
+        # Update value function and policy
+        V[t] = max_value
+        c_policy[t] = max_c
+    end
+
+    return V, c_policy
+end
+
+# Solve the stochastic savings problem
+V, c_policy = value_iteration(T, β, R_mean, R_std, nodes, weights)
+
+# Plot the consumption policy function
+periods = 1:T
+consumption = c_policy
+plot(periods, consumption, xlabel="Period", ylabel="Consumption", legend=false)
+title!("Optimal Consumption Policy")
--- R/8__ELSA_financial.r
+++ R/8__ELSA_financial.r
@@ -1,6 +1,7 @@
 library(data.table)
 library(dplyr)
 library(stringr)
+library(ggplot2)
 
 # Set the correct working directory.
 # Note you need to run the whole script to get this to work.
@@ -15,28 +16,56 @@ ReadAndSetWave <- function(filename) {
     # Read a tab file and set a wave number.
 
     # Extract wave number from filename
-    wave <- str_extract(filename, "wave_\\d{1}")
+    filename_wave <- str_extract(filename, "wave_\\d{1}")
     data <- fread(filename)
     # Set wave in data
-    data[, wave := wave]
+    data[, file_wave := filename_wave]
     return(data)
 }
 
 
 # Compile finance files
-
-financial_files <- dir("../../data/ELSA/elsa_unziped/UKDA-5050-tab/tab/",
+# What is in the finance files?
+all_finance <- dir("../../data/ELSA/elsa_unziped/UKDA-5050-tab/tab/",
     pattern = "wave",
     full.names = TRUE
 ) %>%
-    grep("financial_", ., value = TRUE)
-
-# Join across waves I think. Is there a unique joiner in each household
-lapply(financial_files, \(x) "idauniq" %in% names(fread(x)))
-
-
-all_finance <- lapply(financial_files, ReadAndSetWave) %>%
+    grep("financial_", ., value = TRUE) %>%
+    lapply(ReadAndSetWave) %>%
     rbindlist(fill = TRUE, use.names = TRUE)
+
+# state pension mean to check weekly or monthly
+all_finance[spen_r_i > 0, mean(spen_r_i, na.rm = T)] # cool yes must be weekly
+
+# Mean of annuity income
+all_finance[anin_r_i > 0, mean(anin_r_i, na.rm = T)] # weekly?
+all_finance[, mean(anin_r_i == 0, na.rm = T)] # wow most of sample has none.
+
+
+
+# mean of annuity pension wealth variable
+all_finance[ppinc_bu_s > 0, mean(ppinc_bu_s, na.rm = T)] # not sure how this is different to annuity then?
+
+all_finance[, mean(ppinc_bu_s > 0, na.rm = T)]
+# not sure how this is different to annuity then?
+
+all_finance[]
+
+# anin_r_i anin_p_i wppp_r_i wppp_p_i
+
+# ppinc_bu_s this is called total annuitised income.
+# private plus other?
+# "BU tot annuitised income (priv pen + oth annuity income) - summary var
+# But I don't actually know what that means
+
+
+# let's split these up by cohort, age and year to see if we get any nice differences
+
+# IFS derived variables
+# I should probably use these if i don't expect to make
+# any changes to the raw files
+# these have demographic info and some other stuff which is useful,
+# doesn't seem to have income variables etc though.
 
 all_ifs <- dir("../../data/ELSA/elsa_unziped/UKDA-5050-tab/tab/",
     pattern = "wave",
@@ -46,6 +75,22 @@ all_ifs <- dir("../../data/ELSA/elsa_unziped/UKDA-5050-tab/tab/",
     lapply(ReadAndSetWave) %>%
     rbindlist(fill = TRUE, use.names = TRUE)
 
+# Add interview date
+all_ifs[, intdate := paste0(intdaty, "-", intdatm)]
+all_ifs[, intdate := lubridate::ym(intdate)]
+
+hist(all_ifs$intdate,
+    breaks = 50
+)
+
+
+all_finance$file_wave %>% head()
+nrow(all_ifs)
+nrow(all_finance)
+head(all_ifs$wave)
+# I think these are the pension data that is in the pension grid documentation
+# I haven't had a proper look at the code yet though
+# These seem to be more about where the pensions were from in terms of which employer etc
 
 all_pension <- dir("../../data/ELSA/elsa_unziped/UKDA-5050-tab/tab/",
     pattern = "wave",
@@ -56,35 +101,75 @@ all_pension <- dir("../../data/ELSA/elsa_unziped/UKDA-5050-tab/tab/",
     rbindlist(fill = TRUE, use.names = TRUE)
 
 
-fwrite(all_pension, "../../data/ELSA/elsa_to_use/elsa_pensions.csv")
-fwrite(all_ifs, "../../data/ELSA/elsa_to_use/elsa_pensions.csv")
-fwrite(all_finance, "../../data/ELSA/elsa_to_use/elsa_pensions.csv")
+# join ifs, pension and financial by wave and identifier
 
-head(all_finance$idauniq)
-
-full_join(all_pension, all_ifs)
-
-sum(all_pension[wave == "wave_1"]$idauniq %in%
-    all_pension[wave == "wave_2"]$idauniq)
-# does idauniq link across waves
+all_joined_data <- Reduce(
+    function(x, y) full_join(x, y, by = c("file_wave", "idauniq")),
+    list(all_finance, all_ifs)
+)
+# all_pension leave out the pension data set for now
+# since I am not sure I need it.
 
 
+# plot average by wave and age.
+
+annuity_age_wave_means <- all_joined_data[, .(
+    mean_annuity_inc =
+        mean(anin_r_i[anin_r_i > 0], na.rm = T),
+    per_annuity =
+        mean(anin_r_i > 0, na.rm = T)
+), by = .(age, file_wave)]
+head(annuity_age_wave_means$file_wave)
+
+# Plot percentage who have any annuity income against age shaded by wave.
+ggplot(
+    annuity_age_wave_means[
+        age > 60
+    ][file_wave %in% c("wave_1", "wave_8")],
+    aes(x = age, y = per_annuity, colour = file_wave)
+) +
+    geom_point() +
+    geom_line()
+# ok nice it looks like fewer people do have annuities
+# but still a small portion of the sample have any annuity income.
+# can we do a cross tab
+
+ggplot(
+    annuity_age_wave_means[
+        age > 60
+    ][file_wave %in% c("wave_1", "wave_8")],
+    aes(x = age, y = mean_annuity_inc, colour = file_wave)
+) +
+    geom_point() +
+    geom_line()
+
+
+# waves are every two years starting in 2002
+# so 6th wave was 2014 and 4th was 2010
+# we would expect to start to see the difference from the 4th
+# wave onwards then.
 
 
 
 
 
+# ppinc_bu_s - total annuitised income
 
-
-
+all_joined_data[age == 70][, .(annuity_inc = mean(anin_r_i > 0, na.rm = T)),
+    by = .(intdaty)
+]
+# quite variable
+# should i condition on pension wealth?
+# or some other cohort indicator.
 
 # ppen_bu_i - BU private pensions (iappei/iappmo) - value (incl. imputed values)
 # indobyr - year of birth
 # age
 # idauniq - unique wave identifier
 
-all_finance[, .(ppen_bu_i, indobyr, age)]
-
-
-
-all_finance[, ]
+# questions for the data
+# wealth measures
+# pension wealth and household wealth
+# should i plot data by household or individual level?
+# What discount rate should I use to equivalise money across waves?
+#
