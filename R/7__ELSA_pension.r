b6f3a4b4bbaeacc1cbc17dd197b4d1f4b2e04071
Tobias Leigh-Wood <tjleighwood@gmail.com>
2023-06-25 22:04:22 +0100

    latest work

--- R/7__ELSA_pension.r
+++ R/7__ELSA_pension.r
@@ -3,39 +3,45 @@
 library(data.table)
 library(dplyr)
 library(stringr)
+library(readxl)
 
-# Set the correct working directory.
-# Note you need to run the whole script to get this to work.
-# I.e crtl+shift+enter.
-rand_func <- function(x) x
-cur_dir <- getSrcDirectory(rand_func)
-getwd()
-setwd(cur_dir)
-stop() # Remove if running the whole thing through
+source("__ELSA_functions.R")
 
-pension_files <- dir("../../data/ELSA/elsa_unziped/UKDA-5050-tab/tab/",
+pension_grid <- dir("../../data/ELSA/elsa_unziped/UKDA-5050-tab/tab/",
     pattern = "wave",
-    full.names = T
+    full.names = TRUE
 ) %>%
-    grep("pen", ., value = T)
-# There are two types of pension file
-# Lets figure out what the differnces are.
-head(pension_files)
+    grep("pension(_grid|grid)", ., value = T) %>%
+    lapply(ReadAndSetWave)
+
+#  %>%
+# rbindlist(fill = T, use.names = T)
+
+mapply(search_names, pension_grid, "wpapu")
+
+# oh maybe this is when the pension mapping grid becomes useful.
+# get pension grid lookups
+grid_lookups <- read_excel("../../data/ELSA/elsa_unziped/UKDA-5050-tab/mrdoc/excel/5050_wave_2_pension_grid_corresponding_variables.xls")
+names(grid_lookups)
+head(grid_lookups)
+# lets have a look at the excel files.
+# pension variables seem to be different in each
+
+
 
 
 fread(pension_files[[1]]) %>%
     names() %>%
     grep("", ., value = T)
 
+fread("../../data/ELSA/elsa_unziped/UKDA-5050-tab/tab/wave_9_elsa_pensiongrid_eul_v1.tab") %>%
+    names() %>%
+    grep("", ., value = T)
+
 # Join across waves I think. Is there a unique joiner in each household
 lapply(financial_files, \(x) "idauniq" %in% names(fread(x)))
 
-ReadAndSetWave <- function(filename) {
-    wave <- str_extract("../../data/ELSA/elsa_unziped/UKDA-5050-tab/tab/wave_5_financial_derived_variables.tab", "wave_\\d{1}")
-    data <- fread(filename)
-    data[, wave := wave]
-    return(data)
-}
+
 
 all_pension <- lapply(pension_files, ReadAndSetWave) %>%
     rbindlist(fill = T, use.names = T)
