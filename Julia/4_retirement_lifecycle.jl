eff09a8242e03ea9adaef261f2c0744327a5c7c1
Tobias Leigh-Wood <tjleighwood@gmail.com>
2023-07-04 12:22:59 +0100

    clean up, bequests

--- Julia/4_retirement_lifecycle.jl
+++ Julia/4_retirement_lifecycle.jl
@@ -37,13 +37,19 @@ annuity_cost = sum([cum_death_probs[age] * annuity_payment / ((1 + r)^age) for a
 # how do we use this with the grid points
 # maybe this is why having a grid for consumption is better
 
+# now check value annuitising a proportion of wealth
+# prop of wealth to annuitise. 
+# need to solve function for different levels of annuitisied wealth
+
+θ = 0.5
+
 
 # define consumption grid
 asset_grid = [i * grid_gap for i in 1:grid_points]
 
 # asset accumulation
 # assuming constant nominal annuity
-bc(a, a′, annuity) = a * (1 + r) - a′ + annuity
+bc(a, a′, annuity=0, state_pension=5000) = a * (1 + r) - a′ + annuity + state_pension
 # then annuity effects the starting position on the grid. 
 
 
@@ -79,73 +85,85 @@ for i in eachindex(utility_matrix)
     end
 end
 
-
-
-# set terminal values for optimal consumption, value and next assets.
-terminal_value = [u(final_assets) for final_assets in asset_grid]
-
-terminal_age = 110
-start_age = 65
-
-temp_value = zeros(grid_points, terminal_age - start_age)
-
-temp_value[:, terminal_age-start_age] = terminal_value
-
-# each colum is an age
-# each row is the optimal policy function -- i.e assets next period. 
-opt_policy_function = zeros(grid_points, terminal_age - start_age)
-
-
-t = 1
-
-for t in 1:(terminal_age-start_age-1)
-    # age moves one back
-    age = terminal_age - t
-
-    # this is the column in the data frame that refers to that age. 
-    # we work backwards from the terminal age. 
-    value_index = age - start_age
-
-    prob_of_death = (cum_death_probs[value_index+1] - cum_death_probs[value_index]) / (1 - cum_death_probs[value_index])
-
-    ev_g = temp_value[:, value_index+1] * (1 - prob_of_death)
-
-    # Optimal assets are a function of age and cur assets
-    # we are in age already so just need to return 
-
-    # a vector as long as assets, then optimal a prime, and max value as well. 
-    # then we can do something better with it after. 
-
-    outputs = zeros(grid_points, 3)
-    # col 1 is a index
-    # col 2 is value
-    # col 3 is a prime index
-
-    for x in eachindex(asset_grid)
-
-        # Then maximise returning Value and the best asset index
-        obj_g = utility_matrix[x, :] + (ev_g * β)
-
-        max_and_index = findmax(obj_g)
-        # First element is the max
-        # second element is the index that achieves the max
-        # therefore second element is the policy function.
-
-        outputs[x, :] = [x, max_and_index[1], max_and_index[2]]
-    end
-
-    # save value for this period. 
-    temp_value[:, value_index] = outputs[:, 2]
-
-    opt_policy_function[:, value_index] = outputs[:, 3]
-
+for x in 0:500:50
+    println("x = ", x)
 end
 
+# try first a couple of annuitisation values
+# want value if they annuitise 0:500:50 points
 
-plot(temp_value[:, 5])
+solve_life_cycle = function (annuitisation)
+
+    # set terminal values for optimal consumption, value and next assets.
+    terminal_value = [0 for final_assets in asset_grid]
+    # u(final_assets)
+
+    terminal_age = 110
+    start_age = 65
+
+    temp_value = zeros(grid_points, terminal_age - start_age)
+
+    temp_value[:, terminal_age-start_age] = terminal_value
+
+    # each colum is an age
+    # each row is the optimal policy function -- i.e assets next period. 
+    opt_policy_function = zeros(grid_points, terminal_age - start_age)
+
+    t = 1
+    for t in 1:(terminal_age-start_age-1)
+        # age moves one back
+        age = terminal_age - t
+
+        # this is the column in the data frame that refers to that age. 
+        # we work backwards from the terminal age. 
+        value_index = age - start_age
+
+        prob_of_death = (cum_death_probs[value_index+1] - cum_death_probs[value_index]) / (1 - cum_death_probs[value_index])
+
+        ev_g = temp_value[:, value_index+1] * (1 - prob_of_death)
+
+        # Optimal assets are a function of age and cur assets
+        # we are in age already so just need to return 
+
+        # a vector as long as assets, then optimal a prime, and max value as well. 
+        # then we can do something better with it after. 
+
+        outputs = zeros(grid_points, 3)
+        # col 1 is a index
+        # col 2 is value
+        # col 3 is a prime index
+
+        for x in eachindex(asset_grid)
+
+            # Then maximise returning Value and the best asset index
+            obj_g = utility_matrix[x, :] + (ev_g * β)
+
+            max_and_index = findmax(obj_g)
+            # First element is the max
+            # second element is the index that achieves the max
+            # therefore second element is the policy function.
+
+            outputs[x, :] = [x, max_and_index[1], max_and_index[2]]
+        end
+
+        # save value for this period. 
+        temp_value[:, value_index] = outputs[:, 2]
+
+        opt_policy_function[:, value_index] = outputs[:, 3]
+
+    end
+
+    return [temp_value, opt_policy_function]
+end
+
+temp_value, opt_policy_function = solve_life_cycle()
+
+
+
+opt_policy_function
+temp_value
+plot(temp_value[:, 10])
 # Looks correct!
-
-plot(opt_policy_function[:, 90-65])
 
 # Now give starting assets and we can trace asset decumulation
 # whatever y is this period, next period that is x and then we use the function again. 
@@ -167,6 +185,11 @@ for age in 1:(terminal_age-start_age-1)
 end
 
 plot(consumption_path)
+plot(asset_path)
+# also want to return utility
+
+
+
 asset_path[15]
 
 consumption_matrix[300, 500] # This shouldnt be possible
